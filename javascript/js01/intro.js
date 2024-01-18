f4b2e9ce4e5843f2891b40c8525e2101ccad62e3
jaxer20 <119001942+jaxer20@users.noreply.github.com>
2024-01-18 07:19:39 +0300

    js2

---
+++ javascript/js01/intro.js
@@ -0,0 +1,25 @@
+let sayi1=0
+console.log(sayi1);
+console.clear();
+ad="John"
+str1="mrb dunya\"17 ocak"
+let str2='mrb dunyam'
+let str3=`selam dunya ${ad}`
+let str4=new String("merbaha dunya")
+
+console.log(str3);
+console.log(typeof str4,str4);
+console.log(str1[0]);
+str1[0]="ü"
+console.log(str1);
+str1="merhaba fs16"
+console.log(str1);
+
+let kimlik = 234567898
+
+let adSoyad = "John Smith"
+console.log("Personelimiz",adSoyad,"'in tc",kimlik);
+console.log(`Personelimiz ${adSoyad}'in tc: ${kimlik}`);
+
+let x = -3
+console.log(Boolean(x));
