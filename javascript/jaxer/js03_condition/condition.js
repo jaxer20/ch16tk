fd9e18d469264dbbafaeb232f43670076c21aaa8
jaxer20 <119001942+jaxer20@users.noreply.github.com>
2024-01-21 19:58:53 +0300

    overflow

---
+++ javascript/jaxer/js03_condition/condition.js
@@ -0,0 +1,11 @@
+console.log("conditions");
+
+// const num1 = +prompt("sayi giriniz")
+if (num1 < 0) {
+    console.log("Girilen sayi negatiftir")
+  } else if (num1 > 0) {
+    console.log("Girilen sayi pozitiftir")
+  } else {
+    console.log("Girilen sayi 0'a eşittir")
+  }
+
