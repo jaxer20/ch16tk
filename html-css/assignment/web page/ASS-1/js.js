c9a8e1ca7918062347d7a7c3f838c8ed2ac80724
jaxer20 <119001942+jaxer20@users.noreply.github.com>
2024-01-29 13:52:43 +0300

    project3 finished

--- html-css/assignment/web page/ASS-1/js.js
+++
@@ -1,9 +0,0 @@
-// addEventListener
-// lkjdfglşd
-// dfgjsdfg
-let a = 
-console.log(typeof(null));
-let b = 10 / 0
-console.log(b);
-let c = 1 + 0.2
-console.log(c);
---
+++ html-css/assignments-projects/1-assignment/ass01-web page/ASS-1/js.js
@@ -0,0 +1,9 @@
+// addEventListener
+// lkjdfglşd
+// dfgjsdfg
+let a = 
+console.log(typeof(null));
+let b = 10 / 0
+console.log(b);
+let c = 1 + 0.2
+console.log(c);
